602dd5b4f8e157076942e832cf6ad51a9b54348d
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-08-12 16:07:06 -0600

    all functions present and accounted for

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,6 +4,9 @@ module.exports = {
   find,
   findById,
   findSteps,
+  add,
+  update,
+  remove,
 };
 
 function find() {
@@ -11,11 +14,49 @@ function find() {
 }
 
 function findById(id) {
-  return db("schemes").where({ id });
+  return db("schemes")
+    .where({ id })
+    .then((res) => {
+      if (res.length === 0) return null;
+      return res;
+    });
 }
 
 function findSteps(id) {
   return db("steps")
     .leftJoin("schemes", "schemes.id", "steps.scheme_id")
-    .where("schemes.id", "=", id);
+    .where("schemes.id", "=", id)
+    .select(
+      "steps.id",
+      "steps.step_number",
+      "steps.instructions",
+      "schemes.scheme_name"
+    );
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then((id) => {
+      return findById(id);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(() => findById(id));
+}
+
+function remove(id) {
+  return findById(id).then((scheme) => {
+    const oldScheme = [...scheme];
+    return db("schemes")
+      .where({ id })
+      .del()
+      .then(() => {
+        return oldScheme;
+      });
+  });
 }
